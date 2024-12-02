4e17ab854fd4eb4c657ef956ea3eabcf9e440d5e
1-10 <18842839821@163.com>
2024-12-02 19:45:18 +0800

    debug

--- service/cart/internal/svc/servicecontext.go
+++ service/cart/internal/svc/servicecontext.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/gorm"
 	mlog "mall/log"
 	"mall/service/cart/internal/config"
+	"time"
 )
 
 type ServiceContext struct {
@@ -17,12 +18,20 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	log := mlog.NewLog("CartService")
 	ctx := &ServiceContext{Config: c}
 	dsn := "root:2549124159f@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
-
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
-		ctx.DB = nil
 		log.Error(err.Error())
 		return ctx
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Warn(err.Error())
+	} else {
+		sqlDB.SetMaxOpenConns(16)
+		sqlDB.SetMaxIdleConns(8)
+		sqlDB.SetConnMaxLifetime(time.Hour)
 	}
 
 	ctx.DB = db
--- service/order/internal/svc/servicecontext.go
+++ service/order/internal/svc/servicecontext.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm"
 	mlog "mall/log"
 	"mall/service/order/internal/config"
+	"time"
 )
 
 type ServiceContext struct {
@@ -24,12 +25,20 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	log := mlog.NewLog("OrderService")
 	ctx := &ServiceContext{Config: c}
 	dsn := "root:2549124159f@tcp(127.0.0.1:3306)/mall?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
-
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
-		ctx.DB = nil
 		log.Error(err.Error())
 		return ctx
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Warn(err.Error())
+	} else {
+		sqlDB.SetMaxOpenConns(16)
+		sqlDB.SetMaxIdleConns(8)
+		sqlDB.SetConnMaxLifetime(time.Hour)
 	}
 
 	rdb := redis.NewClient(&redis.Options{
